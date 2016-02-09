8b7b85f38a1c4bd995a97f1fbc9b89d710de33dd
Alan Prando <alan@scanboo.com.br>
2016-02-09 20:25:08 -0200

    quick fix

--- recommender-system/src/main/java/br/com/aprando/recommendersystem/controller/FormularioController.java
+++ recommender-system/src/main/java/br/com/aprando/recommendersystem/controller/FormularioController.java
@@ -149,7 +149,7 @@ public class FormularioController {
 			System.setProperty("user.name", "vagrant"); 
 			Runtime rt = Runtime.getRuntime();
             Process proc = rt.exec( 
-					new String[]{ "/home/azureuser/master-thesis-social-recsys/spark-1.4.1-bin-hadoop2.6/bin/spark-submit", "/vagrant/machine-learning-module/make_prediction.py", formBean.getId()} );
+					new String[]{ "/home/azureuser/master-thesis-social-recsys/spark-1.4.1-bin-hadoop2.6/bin/spark-submit", "/home/azureuser/master-thesis-social-recsys/machine-learning-module/make_prediction.py", formBean.getId()} );
             InputStream stderr = proc.getErrorStream();
             InputStreamReader isr = new InputStreamReader(stderr);
             BufferedReader br = new BufferedReader(isr);
